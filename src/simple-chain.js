0ea337c06b7aa004dd4900386b8ec1d293fedacc
vitalikulsha <kulsha_85@mail.ru>
2022-09-04 16:43:47 +0300

    refactor: update code

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -31,10 +31,7 @@ const chainMaker = {
     return currentChain.join('~~');
   }
 };
-/*
+
 module.exports = {
   chainMaker
-};*/
-let str1 = chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain();
-let str2 = '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )';
-console.log(str1 == str2)
+};
