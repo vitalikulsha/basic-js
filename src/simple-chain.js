234792886089f4d9a1da4caaa9c90383f166a0b2
vitalikulsha <kulsha_85@mail.ru>
2022-09-04 16:42:52 +0300

    fix: update remove function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -10,13 +10,13 @@ const chainMaker = {
     return this.chain.length;
   },
   addLink(value) {
-    this.chain.push('( ' + value + ' )');
+    this.chain.push('( ' + String(value) + ' )');
     return this;
   },
   removeLink(position) {
     if(!Number.isInteger(position) || position < 1 || position > this.chain.length){
-      console.log('position = ' + position)
       throw new Error('You can\'t remove incorrect link!');
+      //throw new Error("You can't remove incorrect link!");
     }
     this.chain.splice(position - 1, 1);
     return this;
@@ -26,10 +26,15 @@ const chainMaker = {
     return this;
   },
   finishChain() {
-    return this.chain.join('~~');
+    let currentChain = this.chain;
+    this.chain = [];
+    return currentChain.join('~~');
   }
 };
-
+/*
 module.exports = {
   chainMaker
-};
+};*/
+let str1 = chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain();
+let str2 = '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )';
+console.log(str1 == str2)
