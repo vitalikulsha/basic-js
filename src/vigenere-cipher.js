8d444175a5cce07ab72724d98026a247de053d94
vitalikulsha <kulsha_85@mail.ru>
2022-09-05 16:29:44 +0300

    feat: vigenere ciphering machine has been done:

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,83 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  tabulaRecta = [];
+  
+  isReverse;
+  
+  constructor(isReverse){
+    this.createTable();
+    if(isReverse || arguments[0] === undefined){
+      this.isReverse = false;
+    } else {
+      this.isReverse = true;
+    }
+    
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(str, key) {
+    if(arguments[0] === undefined || arguments[1] === undefined ){
+      throw new Error('Incorrect arguments!');
+    }  
+    const fullKey = key.padEnd(str.length, key).toUpperCase();
+    let encryptArr = [];
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+      if(this.tabulaRecta[0].includes(str.toUpperCase().charAt(i))){
+        const rowIndex = fullKey.charCodeAt(i - count) - 65;
+        const columnIndex = str.toUpperCase().charCodeAt(i) - 65;        
+        encryptArr.push(this.tabulaRecta[rowIndex][columnIndex]);        
+      } else {
+        encryptArr.push(str[i]);
+        count++;
+      }
+    }
+    if(this.isReverse){
+      encryptArr = encryptArr.reverse();
+    }
+    return encryptArr.join('');
+  }
+  
+  decrypt(str, key) {
+    if(arguments[0] === undefined || arguments[1] === undefined ){
+      throw new Error('Incorrect arguments!');
+    }
+    if(this.isReverse){
+      str = str.split('').reverse().join('');
+    }
+    const fullKey = key.padEnd(str.length, key).toUpperCase();
+    let decryptArr = [];
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+      if(this.tabulaRecta[0].includes(str.toUpperCase().charAt(i))){
+        const rowIndex = fullKey.charCodeAt(i - count) - 65;
+        const columnIndex = this.tabulaRecta[rowIndex].indexOf(str[i]);
+        decryptArr.push(this.tabulaRecta[0][columnIndex]);        
+      } else {
+        decryptArr.push(str[i]);
+        count++;
+      }
+    }    
+    return decryptArr.join('');
+  }
+  
+  createTable(){
+    for(let i = 0; i < 26; i++){
+      this.tabulaRecta[i] = [];
+    }
+    const tabulaRectaRow = [];
+    for(let i = 65; i <= 90; i++){
+      tabulaRectaRow.push(String.fromCharCode(i));
+      this.tabulaRecta[0].push(String.fromCharCode(i));
+    }
+    for(let i=0; i < tabulaRectaRow.length - 1; i++){
+      let symbol = tabulaRectaRow[0];
+      tabulaRectaRow.splice(0, 1);
+      tabulaRectaRow.push(symbol);
+      for(let j = 0; j < tabulaRectaRow.length; j++){
+        this.tabulaRecta[i + 1].push(tabulaRectaRow[j]);
+      } 
+    }
   }
 }
 
